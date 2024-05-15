aa48b9c9d1c735045b864ee984423581212a5d2e
PokeFred <Cedric.paelmke@gmx.de>
2024-05-15 17:09:43 +0200

    Update schema.ts

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,29 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { sqliteTable, text, int } from "drizzle-orm/sqlite-core"
 import type { SQLiteTable } from "drizzle-orm/sqlite-core"
 import type { InferSelectModel, InferInsertModel } from "drizzle-orm"
 
-export const test: SQLiteTable = sqliteTable("test", {
-    test: text("test", { length: 255 }),
+export const item: SQLiteTable = sqliteTable("item", {
+    productId: text("product_id", { length: 255 }),
+    name: text("name", { length: 255 }),
+    category: text("category", { length: 255 }),
+    tier: text("tier", { length: 255 }),
+    npcSellPrice: int("npc_sell_price"),
 })
-export type SelectTest = InferSelectModel<typeof test>
-export type InsertTest = InferInsertModel<typeof test>
+export type SelectItem = InferSelectModel<typeof item>
+export type InsertItem = InferInsertModel<typeof item>
+
+export const bazaar: SQLiteTable = sqliteTable("bazaar", {
+    productId: text("product_id", { length: 255 }),
+    buyAmount: int("buy_amount"),
+    buyPrice: int("buy_price"),
+    buyVolume: int("buy_volume"),
+    buyOrders: int("buy_orders"),
+    buyMovingWeek: int("buy_moving_week"),
+    sellAmount: int("sell_amount"),
+    sellPrice: int("sell_price"),
+    sellVolume: int("sell_volume"),
+    sellOrders: int("sell_orders"),
+    sellMovingWeek: int("sell_moving_week"),
+})
+export type SelectBazaar = InferSelectModel<typeof bazaar>
+export type InsertBazaar = InferInsertModel<typeof bazaar>
